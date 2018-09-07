64635861134d5a7212d23e06e70e73fadc62c933
zhoutk <zhoutk@189.cn>
2018-09-07 15:33:32 +0800

    init tslint.

--- dist/bar.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bar = foo;
-function show() {
-    console.log(`bar's value -- ${bar}`);
-}
-exports.show = show;
-//# sourceMappingURL=bar.js.map
--- dist/foo.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const foo = 121243;
-exports.default = foo;
-//# sourceMappingURL=foo.js.map
